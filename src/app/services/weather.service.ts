420cf55d3c7fcf3fd279b9ccb10822c8a68e06d1
nagesh9868 <nageshwarsharma1990@gmail.com>
2023-10-12 12:38:17 +0530

    first commit

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -13,7 +13,7 @@ export class WeatherService {
   constructor(private http:HttpClient) { }
 
   getWeatherData(cityName:string): Observable<weatherData>{
-    return this.http.get<weatherData>(environment.weatherBaseUrl+`/city/${cityName}`, {headers: new HttpHeaders().set(environment.XRapidAPIKeyHeaderName,environment.XRapidAPIKeyHeaderValue).set(environment.XRapidAPIHostHeaderName,environment.XRapidAPIHostHeaderValue)
+    return this.http.get<weatherData>('https://open-weather13.p.rapidapi.com'+`/city/${cityName}`, {headers: new HttpHeaders().set('X-RapidAPI-Key','ffb71d6b16msh1103b68ce7319dap1388c0jsnd18e83054cbe').set('X-RapidAPI-Host','open-weather13.p.rapidapi.com')
       // params: new ().set('/',cityName)
     } )
   }
